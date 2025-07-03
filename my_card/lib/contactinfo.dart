59dc1bfe9ea97a04db3e03f14ead4beac087eebb
Aakrish Lama <al226na@student.lnu.se>
2025-07-03 15:07:32 +0200

    decoration

--- my_card/lib/contactinfo.dart
+++ my_card/lib/contactinfo.dart
@@ -6,6 +6,20 @@ class ContactInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(16),
+      width: 350,
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withValues(),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ]
+      ),
       child: Column(
         children: [
           Text(
