48d8d4fad7c1c8b2e77cf353c366de1470e2e0c5
Aakrish Lama <al226na@student.lnu.se>
2025-07-03 14:30:03 +0200

    adding image with circle avatar widget

--- my_card/lib/main.dart
+++ my_card/lib/main.dart
@@ -11,19 +11,34 @@ class Mecard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Personal card"),
           centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: const Center(
-          child: Text(
-            'Hello, this is my card',
+        body: Center(
+          child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("hello"),
+              Text("my cardey"),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  "assets/imbobbymom.jpg",
+                )
+              ),
+              CircleAvatar(
+                radius: 150,
+                backgroundImage: AssetImage("assets/imbobbymom.jpg"),
+              )
+              
+            ],
           ),
+          )
         ),
       ),
     );
